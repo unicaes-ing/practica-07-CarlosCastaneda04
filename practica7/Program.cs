08821241821676bf3e607497830f5eb7414d31e4
CarlosCastaneda04 <54734121+CarlosCastaneda04@users.noreply.github.com>
2019-10-11 00:11:40 -0600

    Add files via upload

---
+++ practica7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, edades, menores = 0, mayores = 0;
+            Console.WriteLine("Ingrese la cantidad de personas: ");
+            num = int.Parse(Console.ReadLine());
+            int[] cantidad = new int[num];
+            for (int i = 0; i < cantidad.Length; i++)
+            {
+                Console.WriteLine("Ingrese la edad de la persona");
+                edades = int.Parse(Console.ReadLine());
+                if (edades >= 18)
+                {
+                    mayores++;
+                }
+                else
+                {
+                    menores++;
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("Cantidad de mayores: {0}", mayores);
+            Console.WriteLine("Cantidad de menores: {0}", menores);
+            Console.ReadKey();
+        }
+    }
+}
